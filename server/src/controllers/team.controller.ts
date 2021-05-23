d091be1194a922ab9c6701da6bd50d3c431b0230
Nghiem Van Nghia <41871680+getchar49@users.noreply.github.com>
2021-05-24 06:43:56 +0700

    + Them function vao team controller

--- server/src/controllers/team.controller.ts
+++ server/src/controllers/team.controller.ts
@@ -1,8 +1,30 @@
 import * as _ from "lodash";
 import { Request, Response } from "express";
 
-
+import { redisCache, queries } from "./common";
 
 export default {
-  
+  getAllTeam: async (req: Request, res: Response) => {
+    try {
+      const allTeam = models.Team.findAll({ raw: true });
+
+      res.status(200).send({
+        meta: {
+          type: "success",
+          status: 200,
+          message: ""
+        },
+        allTeam
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        meta: {
+          type: "error",
+          status: 500,
+          message: "server error"
+        }
+      });
+    }
+  }  
 };
